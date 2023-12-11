4ab870350ba9c6bab64ed5ed66b5fad3680923ed
Michael West <michael.west@gpisd.org>
2023-12-11 08:40:10 -0600

    end of week 3

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,6 @@
-export const baseUrl = "http://192.168.0.150:3001/";
+export const baseUrl = "http://192.168.10.158:3001/";
 // export const baseUrl = "http://IP_ADDRESS:3001/";
 // find IP_ADDRESS: ipconfig getifaddr en0
+//export const baseUrl = "http:10.167.129.68:3001/";
+
+//export const baseUrl = "http://10.174.128.63:3001/";
