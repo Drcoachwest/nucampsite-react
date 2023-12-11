c26c744043d35ed80f52c49903d496f20a2c4cef
Michael West <michael.west@gpisd.org>
2023-12-11 11:26:02 -0600

    Sending Email

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -26,8 +27,8 @@ const ReservationScreen = () => {
 
   const handleReservation = () => {
     const message = `Number of Campers: ${campers}
-                            \nHike-In? ${hikeIn}
-                            \nDate: ${date.toLocaleDateString("en-US")}`;
+                          \nHike-In? ${hikeIn}
+                          \nDate: ${date.toLocaleDateString("en-US")}`;
     Alert.alert(
       "Begin Search?",
       message,
@@ -43,6 +44,7 @@ const ReservationScreen = () => {
         {
           text: "OK",
           onPress: () => {
+            presentLocalNotification(date.toLocaleDateString("en-US"));
             resetForm();
           },
         },
@@ -61,9 +63,37 @@ const ReservationScreen = () => {
     setShowCalendar(false);
   };
 
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${reservationDate} requested`,
+        },
+        trigger: null,
+      });
+    };
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
+  };
+
   return (
     <ScrollView>
-      <Animatable.View animation="tada" duration={2000} delay={1000}>
+      <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Number of Campers:</Text>
           <Picker
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,6 +1,6 @@
-export const baseUrl = "http://192.168.10.158:3001/";
+//export const baseUrl = "http://192.168.10.158:3001/";
 // export const baseUrl = "http://IP_ADDRESS:3001/";
 // find IP_ADDRESS: ipconfig getifaddr en0
-//export const baseUrl = "http:10.167.129.68:3001/";
+export const baseUrl = "http:10.167.129.84:3001/";
 
-//export const baseUrl = "http://10.174.128.63:3001/";
+//export const baseUrl = "http://10.174.128.62:3001/";
